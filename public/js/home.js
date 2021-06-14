f7d8c3adc62f7ecd4b93e94908d5382d784ca613
= <phduongit2k1@gmail.com>
2021-06-14 22:27:03 +0700

    Design stream tab detail

--- public/js/home.js
+++ public/js/home.js
@@ -26,3 +26,37 @@ btnWork.addEventListener("click", (event) => {
 
 // Default tab is stream
 btnStream.click();
+
+// Add file to post
+const inputFile = document.getElementById("input-file");
+inputFile.addEventListener("change", (event) => {
+    if (event.target.files.length > 0) {
+        const preview = document.createElement("div");
+        preview.setAttribute("id", "preview");
+        
+        const removeFile = document.createElement("span");
+        removeFile.setAttribute("id", "remove-file");
+        removeFile.innerHTML = "&#x2715;";
+
+        const imgOrnament = document.createElement("img");
+        imgOrnament.setAttribute("src", "../public/svg/document.svg");
+        imgOrnament.setAttribute("alt", "File");
+        
+        const fileName = document.createElement("p");
+        fileName.setAttribute("id", "file-name");
+        fileName.innerHTML = event.target.files[0].name;
+
+        preview.appendChild(removeFile);
+        preview.appendChild(imgOrnament);
+        preview.appendChild(fileName);
+
+        const postContent = document.querySelector('textarea[name="post"]');
+        postContent.parentNode.insertBefore(preview, postContent.nextSibling);
+
+        // Remove file
+        removeFile.addEventListener("click", () => {
+            document.getElementById("input-file").value = "";
+            document.getElementById("preview").remove();
+        });
+    }
+});
